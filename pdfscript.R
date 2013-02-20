f51375637fb0b40dcd7e6a857012d68434254133
Sebastian <sebastian.campbell@sydney.edu.au>
2013-02-20 14:53:07 +1100

    First commit. Added first validation Rmd

---
+++ pdfscript.R
@@ -0,0 +1,9 @@
+library(knitr)
+
+FILENAME <- ""
+
+setwd(FILENAME)
+
+knit(paste0(FILENAME, ".Rmd"))
+# You must have pandoc installed: http://johnmacfarlane.net/pandoc/
+shell(paste0("pandoc -o ", FILENAME, ".pdf ", FILENAME, ".md"))
