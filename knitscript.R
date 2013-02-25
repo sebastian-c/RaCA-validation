199ec723af1e66ed91a7099020998d250d436778
Sebastian <sebastian.campbell@sydney.edu.au>
2013-02-25 14:41:55 +1100

    Added HTML option to pdfscript and changed name to knitscript.

--- knitscript.R
+++ knitscript.R
@@ -5,6 +5,11 @@ FILENAME <- ""
 setwd(FILENAME)
 
 knit(paste0(FILENAME, ".Rmd"))
+
+##HTML
+knit2html(paste0(FILENAME, ".md"))
+
+##PDF
 # You must have pandoc installed: http://johnmacfarlane.net/pandoc/
 shell(paste0("pandoc -o ", "\"", FILENAME, ".pdf", "\" ", "\"", FILENAME, ".md", "\""))
 
